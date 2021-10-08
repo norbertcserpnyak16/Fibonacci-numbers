e44b017a0b62c604a6cfff3797dcbb64645853a7
Norbert Cserpnyák <50951721+norbertcserpnyak16@users.noreply.github.com>
2021-10-08 21:20:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] Fibonacci_numbers = new long[91];
+            Fibonacci_numbers[0] = 1; Fibonacci_numbers[1] = 1;
+            for (int i = 2; i < 91; i++)
+            {
+                Fibonacci_numbers[i] = Fibonacci_numbers[i - 2] + Fibonacci_numbers[i - 1];
+            }
+            for (int i = 0; i < 91; i++)
+            {
+                Console.WriteLine($"The {i + 1}. fibonacci-number: {Fibonacci_numbers[i]}");
+            }
+            Console.ReadLine();
+        }
+        /*static int fibonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return fibonacci(n - 2) + fibonacci(n - 1);
+            }
+        }*/
+    }
+}
